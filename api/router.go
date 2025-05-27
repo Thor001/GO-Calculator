d427606843e6a932f5f784988f17a2fdb85d888e
RealMide <t5296r@gmail.com>
2025-05-27 17:05:27 -0600

    Update router.go

--- api/router.go
+++ api/router.go
@@ -1,20 +1,10 @@
-package api
+package handler
 
 import (
+	"fmt"
 	"net/http"
-
-	"github.com/jordyvanvorselen/go-templ-htmx-vercel-template/controller"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/users", controller.UserHandler{}.Index)
-
-	e.ServeHTTP(w, r)
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
